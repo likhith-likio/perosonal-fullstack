10e22220f96e044508618d730744c6482ae5f220
likhith-likio <95698772+likhith-likio@users.noreply.github.com>
2025-07-12 07:39:55 -0600

    router implemented

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
-import DashboardPage from "./pages/DashboardPage";
-import LoginPage from "./pages/LoginPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <>
-      <LoginPage />
-      {/* <DashboardPage/> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
